28c8db4db1dc3d6ee49aff6f13289e6d449c0c22
jpsantostefano <jpsantostefano@gmail.com>
2023-07-04 20:17:17 +0100

    Project ready to review

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import gspread
 from google.oauth2.service_account import Credentials
+from datetime import date
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -12,44 +13,81 @@ GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open("caloriesCounter")
 
 list = SHEET.worksheet("data")
-data = list.get_all_values()
 
-food_list = {}
-print("Welcome to the calories and protein counter\n")
+option = None
+table = []
+record = None
 
-name = input("Escribe tu nombre: ")
+print("\nWelcome to the BODY-MASS INDEX program\n")
 
-print(f"\nHi {name}, choose one of the options\n")
-print("1- Add new food to the data base")
-print("2- Show full list")
-print("3- Calculate your body mass index\n")
+name = input("What's your name?: ")
 
-option = input("Option: ")
-if option == "1":
-    food = input("Write the name of the food: ")
-    calories = int(input("How many calories have you eaten?: "))
-    food_list[food] = calories
-    print(food_list)
-elif option == "2":
-    print(data)
-elif option == "3":
-    weight = int(input("What's your weight in kg?: "))
-    height = float(input("What's your height in cm: "))
-    imc = round(weight/((height/100)**2),2)
-    if imc < 18.49:
-        print(f"\nYour body-mass index is {imc} and you are in lLOW WEIGHT category\n")
-    elif imc >= 18.5 and imc <=24.49:
-        print(f"\nYour body-mass index is {imc} and you are in NORMAL RANGE category\n")
-    elif imc >= 25 and imc <= 29.99:
-        print(f"\nYour body-mass index is {imc} and you are in OVERWEIGHT category\n")
+print(f"\nHi {name}, select one of the option:\n")
+
+while option != 3:
+    print("\n1- Calculate your body mass index")
+    print("2- See your history")
+    print("3- Exit\n")
+
+    option = input("Select an option: ")
+    if option == "1":
+        while True:
+            try:
+                weight = float(input("\nWhat's your weight in kg?: "))
+                break
+            except ValueError:
+                print("\nPlease, insert a right value\n")
+        while True:    
+            try:
+                height = float(input("What's your height in cm?: "))
+                break
+            except ValueError:
+                print("\nPlease, insert a right value\n")
+                
+        imc = round(weight/((height/100)**2),2)
+        if imc < 18.49:
+            print(f"\nYour body-mass index is {imc} and you are in the lLOW WEIGHT category\n")
+        elif imc >= 18.5 and imc <=24.49:
+            print(f"\nYour body-mass index is {imc} and you are in the NORMAL RANGE category\n")
+        elif imc >= 25 and imc <= 29.99:
+            print(f"\nYour body-mass index is {imc} and you are in the OVERWEIGHT category\n")
+        else:
+            print(f"\nYour body-mass index is {imc} and you are in the OBESE category\n") 
+        print("****BODY-MASS INDEX CLASIFICATION****\n")
+        print("Low weight: Less than 18.5")
+        print("Normal range: 18.5 to 24.99")
+        print("Overweight: 25 to 29.99")
+        print("Obese: more than 30\n")
+
+        
+        while record != "n" and record !="y":
+            record = input("Do you want to add this record? Y/N: ")
+            record = record.lower()
+            
+            if record == "y":
+                day = date.today()
+                today = day.strftime("%d/%m/%Y")
+                table.append(today)
+                table.append(imc)
+                list.append_row(table)
+                table = []
+                print("Your body-mass index has been saved on the spreadsheet")
+                break
+            elif record == "n":
+                break
+            else:
+                print("\nInvalid option. Please write the letter 'Y' to save the record or 'N' to go back to the main menu\n")
+            
+
+    elif option == "2":
+        data = list.get_all_values()
+        data = data[1:]
+        print("   DATE           BODY-MASS INDEX")
+        for row in data:
+            print(row[0],"          ",row[1])
+    elif option == "3":
+        break    
     else:
-        print(f"\nYour body-mass index is {imc} and you are in OBESE category\n") 
-    print("****BODY-MASS INDEX CLASIFICATION****\n")
-    print("Low weight: Less than 18.5")
-    print("Normal range: 18.5 to 24.99")
-    print("Overweight: 25 to 29.99")
-    print("Obese: more than 30")
-else:
-    print("Invalid option")
+        print("\nPlease, insert a valid option\n")
 
 
